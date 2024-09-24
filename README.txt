364322220e7959d89eb85d4bf2acc11a1a9aab7e
Shushizinho <avlismana@gmail.com>
2024-09-24 18:12:20 +0100

    test commit

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
+## Projeto Surgical Appointment And Resource Management
+
 
 Projeto exemplo/template em ASP.NET Core 5.0 API para demonstrar como usar/aplicar (alguns conceitos de) DDD nesta tecnologia e usando também Entity Framework.
 Adota um estilo arquitetural Onion.
@@ -13,4 +15,3 @@ Por simplicidade, empacotei fisicamente (packages) estas coisas por agregado.
 Como é óbvio, outras alternativas de empacotamento são aceitáveis e (se calhar) desejáveis.
 
 Também reconheço que podem ser introduzidas algumas melhorias interessantes com relativamente pouco esforço (mas de momento não tenho tempo).
-
--- obj/Debug/netcoreapp5.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/netcoreapp5.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ee8fd37f272f85070e2c6589845bf60ff95827")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992f7cd8de63bac1b71b548980ed91be8c0a791b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
