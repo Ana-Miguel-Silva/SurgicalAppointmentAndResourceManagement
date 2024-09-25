7f5341b7d1bbbb4b41d089b504e43631cdc2b1bb
Shushizinho <avlismana@gmail.com>
2024-09-25 18:14:09 +0100

    fixing error with package name and processes stil runnig

--- obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fce40568faa7253703dfb4f9ed850bbe24c777f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0faeb9637b90369dcb4de95895797c1dbdf6ba79")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
