fce40568faa7253703dfb4f9ed850bbe24c777f1
Shushizinho <avlismana@gmail.com>
2024-09-25 14:36:01 +0100

    test fixing error

--- obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30888442373bcedc901658e866e71996dd037ff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d0d6f64ccbce3962b97cfc36ac44ccfd87d90a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
