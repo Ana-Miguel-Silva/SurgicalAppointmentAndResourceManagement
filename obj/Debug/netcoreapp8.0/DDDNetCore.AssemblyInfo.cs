728123b87ab2d8533090be003c6e0d8f7bdacd46
Shushizinho <avlismana@gmail.com>
2024-10-03 17:52:50 +0100

    update logical view levels 1,2 and 3

--- obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8acade995acd6827ed10195e32e69a3a1a5ce6b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8926f77035435f0d5b9b28b1e395eaa71cac2df4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
