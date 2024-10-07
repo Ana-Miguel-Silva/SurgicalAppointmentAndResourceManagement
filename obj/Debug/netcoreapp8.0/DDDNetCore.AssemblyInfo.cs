3047b32826907870f804d3ca2ef24e92759c2f44
1221148-isep <1221148@isep.ipp.pt>
2024-10-07 10:04:36 +0100

    add problematic files to gitignore

--- obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/netcoreapp8.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ed3119f6e9a54b2e1c9f2dc9bf2b776f74db4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2123e5b1788158927a24a7bc576a8f07fc07f097")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
