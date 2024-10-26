ffa1c59f6ba758b1b702295d5b18de10e3425de2
Ti4goS4ntos <1221003@isep.ipp.pt>
2024-10-26 17:34:18 +0100

    More changes of the structure of the project

--- ApplicationService/StaffService.cs
+++ ApplicationService/StaffService.cs
@@ -3,7 +3,6 @@ using DDDSample1.Domain.Shared;
 using DDDSample1.Domain.Users;
 using DDDSample1.Domain.Staff;
 
-
 namespace DDDSample1.ApplicationService.Staff
 {
     public class StaffService
@@ -33,7 +32,7 @@ namespace DDDSample1.ApplicationService.Staff
             List<StaffDto> listDto = list.ConvertAll<StaffDto>(staff =>
                 new(staff.Id.AsGuid(), staff.Name, staff.Email, staff.PhoneNumber, staff.Role, staff.Specialization, staff.AvailabilitySlots, staff.StaffId, staff.LicenseNumber));
             if (name != null) listDto = listDto.Where(x => x.Name.Equals(name)).ToList();
-            //if (license != null) listDto = listDto.Where(x => x.LicenseNumber.Equals(license)).ToList();
+            if (license != null) listDto = listDto.Where(x => x.LicenseNumber.Equals(license)).ToList();
             if (phone != null) listDto = listDto.Where(x => x.PhoneNumber.Equals(phone)).ToList();
             if (specialization != null) listDto = listDto.Where(x => x.Specialization.Equals(specialization)).ToList();
             if (role != null) listDto = listDto.Where(x => x.Role.ToUpper().Equals(role.ToUpper())).ToList();
@@ -112,7 +111,7 @@ namespace DDDSample1.ApplicationService.Staff
             //}
             //if(!flag) return null;
             //staff.UpdateSlots(converted);
-            staff.ChangeLicenseNumber(dto.LicenseNumber);
+//            staff.ChangeLicenseNumber(dto.LicenseNumber);
             CheckSpecialization(dto.Specialization);
             staff.UpdateSpecialization(dto.Specialization);
 
