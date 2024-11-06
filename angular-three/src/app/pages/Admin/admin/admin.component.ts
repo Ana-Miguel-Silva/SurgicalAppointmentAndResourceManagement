c8804fdb090f16a49c962b2352a046cc07c8a461
DiogoPereira-1221137 <1221137@isep.ipp.pt>
2024-11-06 12:05:12 +0000

    #35 Implementation of the modal and javascript and css

--- angular-three/src/app/pages/Admin/admin/admin.component.ts
+++ angular-three/src/app/pages/Admin/admin/admin.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  
+  isModalOpen: boolean = false;
+  modalId: string = 'modalId'; 
 
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  onBackdropClick(event: MouseEvent) {
+    this.closeModal(); // Fecha o modal ao clicar fora do conteúdo
+  }
 }
