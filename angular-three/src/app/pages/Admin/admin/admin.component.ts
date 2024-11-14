af0462dd06f4099b6708bfb30e2754714048c124
DiogoPereira-1221137 <1221137@isep.ipp.pt>
2024-11-14 16:28:35 +0000

    #35 Implementation of sweet alert and add patient profile

--- angular-three/src/app/pages/Admin/admin/admin.component.ts
+++ angular-three/src/app/pages/Admin/admin/admin.component.ts
@@ -5,7 +5,9 @@ import { ModalService } from './modal.service';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { AuthService } from '../../../Services/auth.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
+// or via CommonJS
 @Component({
   selector: 'app-admin',
   standalone: true,
@@ -17,31 +19,44 @@ export class AdminComponent {
 
   private staffUrl = "https://localhost:5001/api/Staff";
 
-  constructor(private fb: FormBuilder, private modalService: ModalService, 
+  constructor(private fb: FormBuilder, private modalService: ModalService,
     private http: HttpClient, private authService: AuthService, private router: Router) {
     // Define os controles do formulário com validações
     this.myForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      username: ['', Validators.required],
-      city: ['', Validators.required],
-      state: ['', Validators.required],
-      zip: ['', Validators.required],
-      agree: [false, Validators.requiredTrue],
-      inputTag: new FormControl('')  
+      name: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      userEmail: ['', [Validators.required, Validators.email] ],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+      gender: ['', Validators.required],
+      inputTag: [''], // Controlador para o campo de "Allergies"
+      emergencyContactName: ['', Validators.required],
+      emergencyContactEmail: ['', [Validators.required, Validators.email]],
+      emergencyContactPhone: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+      agree: [false, Validators.requiredTrue]
     });
+    
     this.staffForm = this.fb.group({});
+    this.patientForm = this.fb.group({});
   }
 
   selectedStaffId: number | null = null;
+  selectedPatientEmail: string | null = null;
 
   selectStaff(id: number) {
     this.selectedStaffId = this.selectedStaffId === id ? null : id;
   }
 
 
+  selectPatient(email: string) {
+    this.selectedPatientEmail = this.selectedPatientEmail === email ? null : email;
+  }
+
+
+
   myForm: FormGroup;
   staffForm: FormGroup;
+  patientForm: FormGroup;
   tags: string[] = [];  // Array para armazenar as tags
   successMessage: string | null = null;
   errorMessage: string | null = null;
@@ -49,7 +64,7 @@ export class AdminComponent {
   staffsProfiles: any[] = [];
   searchTerm: string = '';
   filterField: string = '';
- 
+
   /*onBackdropClick(event: MouseEvent) {
     this.closeModal(); // Fecha o modal ao clicar fora do conteúdo
   }*/
@@ -65,17 +80,107 @@ export class AdminComponent {
   isModalOpen(modalId: string): boolean {
     return this.modalService.isModalOpen(modalId);
   }
- 
+
 
 
   // Método para submeter o formulário
   onSubmit() {
+
+
+    const token = this.authService.getToken();
+
+
+    if (!token) {
+      alert('You are not logged in!');
+      return;
+    }
+
     if (this.myForm.valid) {
-      console.log('Form Submitted!', this.myForm.value);
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+
+      this.myForm.patchValue({ inputTag: this.tags });
+
+      // Obtém os valores do formulário
+      const formData = this.myForm.value;
+      const apiUrl = 'https://localhost:5001/api/Patients/register'; // URL da sua API
+
+      // Enviar os dados diretamente com HttpClient
+      this.http.post(apiUrl, formData,{ headers })
+      .subscribe(
+        response => {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "success",
+            title: "Formulário submetido com sucesso"
+          });
+          // Redefinir o formulário após o envio, se desejado
+          this.myForm.reset();
+        },
+        error => {
+          console.error("Erro ao submeter o formulário", error);
+        }
+      );
+
     } else {
-      this.myForm.markAllAsTouched();  // Marca todos os campos para mostrar feedback de validação
+      // Marque todos os campos como tocados para exibir as mensagens de erro
+      this.myForm.markAllAsTouched();
+      console.log("Formulário inválido");
+    }
+
+  }
+
+  /*
+  onSubmit() {
+    if (this.myForm.valid) {
+      // Obtém os valores do formulário
+      const formData = this.myForm.value;
+      const apiUrl = 'https://sua-api.com/endpoint'; // URL da sua API
+
+      // Enviar os dados diretamente com HttpClient
+      this.http.post(apiUrl, formData).subscribe(
+        response => {
+          console.log("Formulário submetido com sucesso", response);
+          // Redefinir o formulário após o envio, se desejado
+          this.myForm.reset();
+        },
+        error => {
+          console.error("Erro ao submeter o formulário", error);
+        }
+      );
+
+    } else {
+      // Marque todos os campos como tocados para exibir as mensagens de erro
+      this.myForm.markAllAsTouched();
+      console.log("Formulário inválido");
     }
   }
+  
+  */
+
+  onSubmitPatient(){}
+
+
+
+
+
+
+
+
+
   onSubmitStaff(){}
   deactivateStaff(){
     const token = this.authService.getToken();
@@ -92,7 +197,7 @@ export class AdminComponent {
       window.alert("Please select a staff member to deactivate.");
     } else {
       console.log(`Deactivating staff ID: ${this.selectedStaffId}`);
-      this.http.delete<string>(`${this.staffUrl}/${this.selectedStaffId}`, { headers })  
+      this.http.delete<string>(`${this.staffUrl}/${this.selectedStaffId}`, { headers })
       .subscribe({
         next: (response) => {
           console.log(response);
@@ -105,7 +210,7 @@ export class AdminComponent {
       this.getAllstaffsProfiles();
     }
   }
-  
+
   viewStaff(){
     const token = this.authService.getToken();
 
@@ -121,7 +226,7 @@ export class AdminComponent {
       window.alert("Please select a staff member to view.");
     } else {
       console.log(`Viewing staff ID: ${this.selectedStaffId}`);
-      this.http.get<string>(`${this.staffUrl}/${this.selectedStaffId}`, { headers })  
+      this.http.get<string>(`${this.staffUrl}/${this.selectedStaffId}`, { headers })
       .subscribe({
         next: (response) => {
           console.log(response);
@@ -135,7 +240,7 @@ export class AdminComponent {
   }
 
 
-  
+
 
   // Método para adicionar uma tag ao array
   addTag(event: KeyboardEvent) {
@@ -152,8 +257,8 @@ export class AdminComponent {
       inputTagControl?.setValue('');  // Limpa o campo de input usando setValue
     }
   }
-  
-  
+
+
 
   // Método para remover uma tag pelo índice
   removeTag(index: number) {
@@ -161,7 +266,7 @@ export class AdminComponent {
   }
 
 
-  
+
   ngOnInit() {
     const token = this.authService.getToken();
 
@@ -198,9 +303,9 @@ export class AdminComponent {
   .set(this.filterField.toLowerCase().replace(/\s+/g, '')  // Converte para minúsculas e remove os espaços
 , this.searchTerm || '');
 
-    
-  
-    this.http.get<any[]>('https://localhost:5001/api/Patients/search', { headers, params })  
+
+
+    this.http.get<any[]>('https://localhost:5001/api/Patients/search', { headers, params })
       .subscribe({
         next: (response) => {
           this.patientsProfiles = response;
@@ -230,9 +335,9 @@ export class AdminComponent {
   .set(this.filterField.toLowerCase().replace(/\s+/g, '')  // Converte para minúsculas e remove os espaços
 , this.searchTerm || '');
 
-    
-  
-    this.http.get<any[]>(`${this.staffUrl}`, { headers, params })  
+
+
+    this.http.get<any[]>(`${this.staffUrl}`, { headers, params })
       .subscribe({
         next: (response) => {
           this.staffsProfiles = response;
