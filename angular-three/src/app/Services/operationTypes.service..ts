a2c7c090f4d9e566f522d67e7cf5288531ca23af
Shushizinho <avlismana@gmail.com>
2024-11-19 13:32:21 +0000

    #50 operation list and view

---
+++ angular-three/src/app/Services/operationTypes.service..ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+@Injectable()
+export class OperationTypesService {
+  private apiUrl = 'https://localhost:5001/api/OperationTypes';
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+getAllOperationTypes(): Observable<any[]> {
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<any[]>(`${this.apiUrl}`, { headers });
+}
+
+createOperationTypes(operationTupe : any): Observable<any> {
+  const token = this.authService.getToken();
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+  return this.http.post(`${this.apiUrl}`, operationTupe, { headers })
+}
+
+getSearchOperationTypes(params : any): Observable<any[]> {
+  const token = this.authService.getToken();
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+  return this.http.get<any[]>(`${this.apiUrl}/search`, { headers, params });
+}
+
+getOperationTypeById(id: string): Observable<any> {
+  const token = this.authService.getToken();
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+  return this.http.get<any>(`${this.apiUrl}/${id}`, { headers });
+}
+
+
+
+
+
+}
