de9c6706361a9d091596b4add095154ef569db34
Shushizinho <avlismana@gmail.com>
2025-01-04 17:36:59 +0000

    #102 update IPs to VM

--- angular-three/src/environments/environment.ts
+++ angular-three/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    //apiBaseUrl: "https://20.82.142.194:5001/api",
-    apiBaseUrl: "https://localhost:5001/api",
-    apiMongoUrl: "http://localhost:4000/api",
-    //apiMongoUrl: "http://20.82.142.194:4000/api",
+    apiBaseUrl: "https://20.82.142.194:5001/api",
+    //apiBaseUrl: "https://localhost:5001/api",
+    //apiMongoUrl: "http://localhost:4000/api",
+    apiMongoUrl: "http://20.82.142.194:4000/api",
   };
