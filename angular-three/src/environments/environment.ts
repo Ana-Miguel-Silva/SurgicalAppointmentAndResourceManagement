9eaf75b95c60de5f0284b4c7b9c46b12b119f657
Shushizinho <avlismana@gmail.com>
2025-01-04 22:32:15 +0000

    #102 update stats page teste3

--- angular-three/src/environments/environment.ts
+++ angular-three/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     //apiBaseUrl: "https://20.82.142.194:5001/api",
-    apiBaseUrl: "https://51.120.112.94:4200/5001/api",
+    apiBaseUrl: "http://51.120.112.94:4200/5001/api",
     //apiBaseUrl: "https://localhost:5001/api",
     //apiMongoUrl: "http://localhost:4000/api",
     //apiMongoUrl: "http://20.82.142.194:4000/api",
